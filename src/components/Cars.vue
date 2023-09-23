beb5ea1f783c53ace932b0fedf04d75762a6d16b
Gandobilis <lashadeveloper@gmail.com>
2023-09-23 16:39:06 +0400

    nav menu margin bug fixed.

--- src/components/Cars.vue
+++ src/components/Cars.vue
@@ -6,13 +6,13 @@ import {onMounted} from "vue";
 const {data, error, isLoading, fetchData} = useCars();
 
 onMounted(() => {
-  fetchData('https://usabidfax.netlify.app/.netlify/functions/json-server?_page=1&_limit=9');
+  fetchData('http://localhost:3000/cars?_page=1&_limit=9');
 })
 </script>
 
 <template>
   <div v-if="data" class="grid grid-cols-1 sm:max-lg:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4">
-    <Car v-for="(car, index) in data.items" :car="car" :key="index"/>
+    <Car v-for="(car, index) in data" :car="car" :key="index"/>
   </div>
   <h1 v-else>Loading...</h1>
 </template>
