354a650cd9920c4efa74757d1c5f2c177d6a547d
Gandobilis <lashadeveloper@gmail.com>
2023-09-23 15:58:41 +0400

    vin, make, model, _page, _limit, _sort, _order functionalities added to json server.

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -2,11 +2,42 @@
 const data = require('./data.json'); // Import your JSON data
 
 exports.handler = async (event, context) => {
+    const queryParams = event.queryStringParameters || {};
+    const { vin, make, model, _page, _limit, _sort, _order } = queryParams;
+
+    // Apply filters
+    let filteredItems = [...data.items];
+
+    if (vin) {
+        filteredItems = filteredItems.filter(item => item.vin === vin);
+    }
+
+    if (make) {
+        filteredItems = filteredItems.filter(item => item.make === make);
+    }
+
+    if (model) {
+        filteredItems = filteredItems.filter(item => item.model === model);
+    }
+
+    // Apply sorting
+    if (_sort) {
+        const order = _order === 'desc' ? -1 : 1;
+        filteredItems.sort((a, b) => (a[_sort] > b[_sort] ? order : -order));
+    }
+
+    // Apply pagination
+    if (_page && _limit) {
+        const startIndex = (_page - 1) * _limit;
+        const endIndex = startIndex + parseInt(_limit);
+        filteredItems = filteredItems.slice(startIndex, endIndex);
+    }
+
     return {
         statusCode: 200,
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({ items: filteredItems }),
     };
 };
